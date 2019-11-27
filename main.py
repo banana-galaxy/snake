a11654f4021e6d1a12f2501fb3b7089eb67b19f1
banana-galaxy <cawasp@gmail.com>
2019-11-26 18:19:46 -0800

    added list for snake coordinates and changed grid color

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ vel_x = 0
 vel_y = 0
 add_length = 0
 
-snake_x = random.randrange(len(grid))
-snake_y = random.randrange(len(grid))
-print(snake_x, snake_y)
+snake_body = [[]]
+snake_body[0].append(random.randrange(len(grid)))
+snake_body[0].append(random.randrange(len(grid)))
+print(snake_body[0][0], snake_body[0][1])
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My Game")
@@ -28,6 +29,7 @@ clock = pygame.time.Clock()
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
@@ -53,12 +55,20 @@ while not done:
 
     print(vel_x, vel_y)
 
-
     # --- Game logic
-    grid[snake_x][snake_y] = 0
+    snake_body.append([])
+    snake_body[len(snake_body) - 1].append(snake_body[len(snake_body) - 2][0] + vel_x)
+    snake_body[len(snake_body) - 1].append(snake_body[len(snake_body) - 2][1] + vel_y)
+    if add_length:
+        add_length = 0
+        pass
+    else:
+        snake_body.pop(0)
+
+    '''grid[snake_x][snake_y] = 0
     snake_x += vel_x
     snake_y += vel_y
-    grid[snake_x][snake_y] = 1
+    grid[snake_x][snake_y] = 1'''
 
     # --- Screen-clearing code
     screen.fill(WHITE)
@@ -68,19 +78,17 @@ while not done:
     # - Drawing the food
 
     # - Drawing the snake
-    for x in range(len(grid)):
-        for y in range(len(grid[x])):
-            if grid[x][y]:
-                pygame.draw.rect(screen, BLACK, [x*block_size, y*block_size, 10, 10], 0)
+    for cell in snake_body:
+        pygame.draw.rect(screen, BLACK, [cell[0]*block_size, cell[1]*block_size, 10, 10], 0)
 
     # - Grid should always be on top of everything, so this is the last thing to be drawn
-    gamefunctions.draw_grid(pygame, screen, [screen_width, screen_height], grid_size, block_size, BLACK)
+    gamefunctions.draw_grid(pygame, screen, [screen_width, screen_height], grid_size, block_size, WHITE)
 
     # --- Update the screen
     pygame.display.flip()
 
     # --- Limit to 60 frames per second
-    clock.tick(6)
+    clock.tick(10)
 
 # Close the window and quit.
 pygame.quit()
