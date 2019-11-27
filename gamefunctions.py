bc61cb9c0566a7113742417a77fe6205ba436b6d
Alexander Patrushin <cawasp@gmail.com>
2019-11-27 14:08:04 -0800

    added fully functional collision mechanics and a gameover screen as well as a way to restart the game

--- gamefunctions.py
+++ gamefunctions.py
@@ -9,8 +9,27 @@ def initiate_grid(window_size, block_size):
 
     return grid
 
+
 def draw_grid(pygame, screen, screenSize, grid_size, block_size, color):
     for x in range(grid_size):
         pygame.draw.line(screen, color, [block_size*x, 0], [block_size * x, screenSize[1]])
     for y in range(grid_size):
         pygame.draw.line(screen, color, [0, block_size*y], [screenSize[0], block_size * y])
+
+
+def collide(grid_size, snake, velocities):
+    head = len(snake)-1
+    next_head_x = snake[head][0]+velocities[0]
+    next_head_y = snake[head][1] + velocities[1]
+
+    # snake collision
+    for cell in range(len(snake)):
+        if cell != head:
+            if snake[cell][0] == next_head_x and snake[cell][1] == next_head_y:
+                return True
+
+    # wall collision
+    if next_head_x >= grid_size or next_head_x < 0:
+        return True
+    if next_head_y >= grid_size or next_head_y < 0:
+        return True
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame, gamefunctions, random
 pygame.init()
 
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+WHITE = (244,244,244)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 screen_width = 500
@@ -16,6 +16,9 @@ vel_x = 0
 vel_y = 1
 direction_change = False
 add_length = 0
+gameover_pic = pygame.image.load("g_o_t.png")
+gameover = False
+restart = False
 
 foods = [[]]
 foods[0].append(random.randrange(len(grid)))
@@ -24,10 +27,9 @@ foods[0].append(random.randrange(len(grid)))
 snake_body = [[]]
 snake_body[0].append(int(len(grid)/2))
 snake_body[0].append(int(len(grid)/2))
-print(snake_body[0][0], snake_body[0][1])
 
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Snake")
 clock = pygame.time.Clock()
 
 
@@ -39,7 +41,9 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.KEYDOWN:
-            if not direction_change:
+            if event.key == pygame.K_SPACE:
+                restart = True
+            if not direction_change and not gameover:
                 if event.key == pygame.K_RIGHT:
                     if vel_y != 0:
                         vel_y = 0
@@ -61,49 +65,58 @@ while not done:
                         vel_y = 1
                         direction_change = True
 
-    print(vel_x, vel_y)
+    if restart:
+        restart = False
+        gameover = False
+        snake_body = [[]]
+        snake_body[0].append(int(len(grid) / 2))
+        snake_body[0].append(int(len(grid) / 2))
+        vel_x = 0
+        vel_y = 1
 
-    # --- Game logic
-    snake_body.append([])
-    snake_body[len(snake_body) - 1].append(snake_body[len(snake_body) - 2][0] + vel_x)
-    snake_body[len(snake_body) - 1].append(snake_body[len(snake_body) - 2][1] + vel_y)
-    if snake_body[len(snake_body)-1][0] == foods[len(foods)-1][0]:
-        if snake_body[len(snake_body)-1][1] == foods[len(foods)-1][1]:
-            foods.append([])
-            foods[len(foods)-1].append(random.randrange(len(grid)))
-            foods[len(foods)-1].append(random.randrange(len(grid)))
-            foods.pop(0)
-            add_length = 1
-    if add_length:
-        add_length = 0
-        pass
+
+    gameover = gamefunctions.collide(grid_size, snake_body, (vel_x, vel_y))
+    if not gameover:
+        # --- Game logic
+        snake_body.append([])
+        snake_body[len(snake_body) - 1].append(snake_body[len(snake_body) - 2][0] + vel_x)
+        snake_body[len(snake_body) - 1].append(snake_body[len(snake_body) - 2][1] + vel_y)
+        if snake_body[len(snake_body)-1][0] == foods[len(foods)-1][0]:
+            if snake_body[len(snake_body)-1][1] == foods[len(foods)-1][1]:
+                foods.append([])
+                foods[len(foods)-1].append(random.randrange(len(grid)))
+                foods[len(foods)-1].append(random.randrange(len(grid)))
+                foods.pop(0)
+                add_length = 1
+        if add_length:
+            add_length = 0
+            pass
+        else:
+            snake_body.pop(0)
+
+        # --- Screen-clearing code
+        screen.fill(WHITE)
+
+
+        # --- Drawing code
+
+        # - Drawing the snake
+        for cell in snake_body:
+            pygame.draw.rect(screen, BLACK, [cell[0]*block_size, cell[1]*block_size, 10, 10], 0)
+
+        # - Drawing the grid
+        gamefunctions.draw_grid(pygame, screen, [screen_width, screen_height], grid_size, block_size, WHITE)
+
+        # - Drawing the food
+        for food in foods:
+            pygame.draw.rect(screen, RED, [food[0] * block_size, food[1] * block_size, 10, 10], 0)
+
+        direction_change = False
+        # --- Update the screen
+        pygame.display.flip()
     else:
-        snake_body.pop(0)
-
-    '''grid[snake_x][snake_y] = 0
-    snake_x += vel_x
-    snake_y += vel_y
-    grid[snake_x][snake_y] = 1'''
-
-    # --- Screen-clearing code
-    screen.fill(WHITE)
-
-    # --- Drawing code
-
-    # - Drawing the snake
-    for cell in snake_body:
-        pygame.draw.rect(screen, BLACK, [cell[0]*block_size, cell[1]*block_size, 10, 10], 0)
-
-    # - Drawing the grid
-    gamefunctions.draw_grid(pygame, screen, [screen_width, screen_height], grid_size, block_size, WHITE)
-
-    # - Drawing the food
-    for food in foods:
-        pygame.draw.rect(screen, RED, [food[0] * block_size, food[1] * block_size, 10, 10], 0)
-
-    direction_change = False
-    # --- Update the screen
-    pygame.display.flip()
+        screen.blit(gameover_pic, (0, 0))
+        pygame.display.flip()
 
     # --- Limit to 60 frames per second
     clock.tick(10)
